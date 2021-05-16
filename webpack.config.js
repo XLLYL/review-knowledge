c904d258d5945c2868231b8ff1954e77a7861adc
xiaolei <xiaolei@linklogis.com>
2021-05-16 18:06:35 +0800

    docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'index.js',
-	path: path.resolve(__dirname, 'dist'),
+	path: path.resolve(__dirname, 'docs'),
   },
 	module: {
 		rules: [
