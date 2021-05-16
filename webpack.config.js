3329e3832949492854dc504ed5023d343a4ea2e4
xiaolei <xiaolei@linklogis.com>
2021-05-16 18:09:38 +0800

    适配目录

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'index.js',
-	path: path.resolve(__dirname, 'docs'),
+    filename: 'bundle.js',
+	path: path.resolve(__dirname),
   },
 	module: {
 		rules: [
